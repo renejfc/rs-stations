fcc70d87c55a7651b4f11b64277a70d426276555
René <renejfc@gmail.com>
2024-01-30 20:05:56 +0000

    feat: add primitive UI
    input & label components + utils

---
+++ src/components/ui/Label.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { cn } from '@/utils'
+
+const props = defineProps<{
+  for?: string
+}>()
+</script>
+<template>
+  <label :for="props.for" :class="cn('block text-sm font-medium tracking-tight text-foreground')">
+    <slot />
+  </label>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Input, Label } from '@/components/ui'
+import { ref } from 'vue'
+
+const input = ref('')
+</script>
+
+<template>
+  <div class="container pt-10">
+    <Label for="gen">Gen</Label>
+    <Input id="gen" v-model="input" />
+  </div>
+</template>
